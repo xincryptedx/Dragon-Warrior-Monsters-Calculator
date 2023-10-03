c5a4115ed6323d869d5806b8be3d9f4d6b87b799
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-10-02 20:19:38 -0400

    Import utility to generate data and run on mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import generateRecipesByMonster from "./utilities/generateRecipesByMonster";
+
 function App() {
+  useEffect(() => {
+    generateRecipesByMonster();
+  }, []);
+
   return (
     <div>
       <p>Hello GreatTree!</p>
