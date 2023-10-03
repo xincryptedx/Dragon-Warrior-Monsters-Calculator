258b1403c179df45e747016f16bfa72ed5e81113
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-10-02 20:39:22 -0400

    Remove test useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-import { useEffect } from "react";
-import generateRecipesByMonster from "./utilities/generateRecipesByMonster";
-
 function App() {
-  useEffect(() => {
-    generateRecipesByMonster();
-  }, []);
-
   return (
     <div>
       <p>Hello GreatTree!</p>
